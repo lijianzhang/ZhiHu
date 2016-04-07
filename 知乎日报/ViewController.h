bf345d15558ab31d7c0e9c5771f92c68849191b1
CCOOOOLL <372142750@qq.com>
2016-04-07 21:58:33 +0800

    初始化知乎日报项目

---
+++ 知乎日报/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  知乎日报
+//
+//  Created by Jz on 16/4/7.
+//  Copyright © 2016年 Jz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ 知乎日报/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  知乎日报
+//
+//  Created by Jz on 16/4/7.
+//  Copyright © 2016年 Jz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
